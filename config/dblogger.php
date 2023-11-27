aca77ac1d620c187c7e72719969acc611fe9748f
Alimohammadi20 <alimohi8118@gmail.com>
2023-11-27 15:21:31 +0330

    fixed config keys

--- config/dblogger.php
+++ config/dblogger.php
@@ -1,14 +1,14 @@
 <?php
 return [
-    'enable' => (bool)env('LOGGER_ENABLE', true),
-    'middleware' => env('LOGGER_MIDDLEWARE', ''),
-    'prefix' => env('LOGGER_PREFIX', 'admin'),
-    'asset_url' => env('LOGGER_ASSET_URL', ''),
+    'enable' => (bool)env('DBLOGGER_ENABLE', true),
+    'middleware' => env('DBLOGGER_MIDDLEWARE', ''),
+    'prefix' => env('DBLOGGER_PREFIX', 'admin'),
+    'asset_url' => env('DBLOGGER_ASSET_URL', ''),
     // unit: day
-    'expire' => (int)env('LOGGER_EXPIRE', '7'),
+    'expire' => (int)env('DBLOGGER_EXPIRE', '7'),
     'response_time' => [
-        'min' => (float)env('LOGGER_MAX_RESPONSE_TIME', '1.0'),
-        'max' => (float)env('LOGGER_MIN_RESPONSE_TIME', '0.50'),
+        'min' => (float)env('DBLOGGER_MAX_RESPONSE_TIME', '1.0'),
+        'max' => (float)env('DBLOGGER_MIN_RESPONSE_TIME', '0.50'),
     ],
-    'show_message_limit' => (int)env('LOGGER_MESSAGE_LIMIT', 250)
+    'show_message_limit' => (int)env('DBLOGGER_MESSAGE_LIMIT', 250)
 ];
