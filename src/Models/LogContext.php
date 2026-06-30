f2cfdddcc66bbf9ddc498f083b6443152896a18a
Ali Mohammadi <alimohi8118@gmail.com>
2026-06-30 17:51:04 +0330

    fix: prevent log bytes on context

--- src/Models/LogContext.php
+++ src/Models/LogContext.php
@@ -3,9 +3,25 @@
 namespace Alimi7372\DBLogger\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Casts\Attribute;
 
 class LogContext extends Model
 {
     protected $table = 'log_contexts';
     protected $guarded = ['id'];
+
+    protected function context(): Attribute
+    {
+        return Attribute::make(
+            set: function (mixed $value) {
+                $data = is_array($value) ? $value : json_decode($value, true);
+
+                if (isset($data['bytes'])) {
+                    $data['bytes'] = 'File Uploaded';
+                }
+
+                return json_encode($data);
+            }
+        );
+    }
 }
